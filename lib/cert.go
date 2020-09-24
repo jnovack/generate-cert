1e20228b34f1ed8e112d8e8b2f345d463e24ec38
Justin J. Novack <jnovack@gmail.com>
2020-09-23 21:27:20 -0400

    rewrite

--- lib/cert.go
+++ lib/cert.go
@@ -7,17 +7,19 @@ import (
 	"crypto/rand"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/hex"
 	"encoding/pem"
 	"fmt"
-	"log"
 	"math/big"
 	"net"
+	"strings"
 	"time"
 )
 
-const Version = "0.2"
-
+// Cert structure
 type Cert struct {
+	Certificate x509.Certificate
+
 	Private *pem.Block
 	Public  *pem.Block
 
@@ -25,129 +27,181 @@ type Cert struct {
 	PublicBytes  []byte
 }
 
-type Certs struct {
-	Root, Leaf, Client *Cert
+var caTemplate = x509.Certificate{
+	IsCA:      true,
+	NotBefore: time.Now().UTC(),
+	NotAfter:  time.Now().UTC().Add(367 * 24 * time.Hour),
+
+	KeyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+
+	BasicConstraintsValid: true,
 }
 
-func Generate(hosts []string, org string, validFor time.Duration) (*Certs, error) {
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+var serverTemplate = x509.Certificate{
+	IsCA:      false,
+	NotBefore: time.Now().UTC(),
+	NotAfter:  time.Now().UTC().Add(367 * 24 * time.Hour),
+
+	KeyUsage: x509.KeyUsageDigitalSignature,
+	ExtKeyUsage: []x509.ExtKeyUsage{
+		x509.ExtKeyUsageServerAuth,
+	},
+	BasicConstraintsValid: true,
+}
+
+var clientTemplate = x509.Certificate{
+	IsCA:      false,
+	NotBefore: time.Now().UTC(),
+	NotAfter:  time.Now().UTC().Add(367 * 24 * time.Hour),
+
+	KeyUsage: x509.KeyUsageDigitalSignature,
+	ExtKeyUsage: []x509.ExtKeyUsage{
+		x509.ExtKeyUsageClientAuth,
+	},
+	BasicConstraintsValid: true,
+}
+
+func randomSerialNumber() (*big.Int, *string, error) {
+	serialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
 	if err != nil {
-		return nil, fmt.Errorf("failed to generate serial number: %s", err)
-	}
-	notBefore := time.Now().UTC()
-	notAfter := notBefore.Add(validFor)
-
-	rootTemplate := x509.Certificate{
-		IsCA:         true,
-		SerialNumber: serialNumber,
-		Subject: pkix.Name{
-			Organization: []string{org},
-			SerialNumber: serialNumber.String(),
-		},
-		NotBefore: notBefore,
-		NotAfter:  notAfter,
-
-		KeyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-		ExtKeyUsage: []x509.ExtKeyUsage{
-			x509.ExtKeyUsageServerAuth,
-			x509.ExtKeyUsageClientAuth,
-		},
-		BasicConstraintsValid: true,
+		return nil, nil, fmt.Errorf("failed to generate serial number: %s", err)
 	}
 
-	leafSerialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		log.Fatalf("failed to generate serial number: %s", err)
-	}
-	leafTemplate := x509.Certificate{
-		IsCA:         false,
-		SerialNumber: leafSerialNumber,
-		Subject: pkix.Name{
-			Organization: []string{org},
-			SerialNumber: leafSerialNumber.String(),
-		},
-		NotBefore: notBefore,
-		NotAfter:  notAfter,
+	src := serialNumber.Bytes()
+	dst := make([]byte, hex.EncodedLen(len(src)))
+	hex.Encode(dst, src)
+	lastFour := fmt.Sprintf("%s", strings.ToUpper(string(dst[len(dst)-4:])))
 
-		KeyUsage: x509.KeyUsageDigitalSignature,
-		ExtKeyUsage: []x509.ExtKeyUsage{
-			x509.ExtKeyUsageServerAuth,
-		},
-		BasicConstraintsValid: true,
+	return serialNumber, &lastFour, nil
+}
+
+// GenerateRoot generates a root certificate, returns cert
+func GenerateRoot(fields pkix.Name) (*Cert, error) {
+	if fields.CommonName == "" {
+		fields.CommonName = fields.Organization[0]
 	}
 
-	clientSerialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		log.Fatalf("failed to generate serial number: %s", err)
-	}
-	clientTemplate := x509.Certificate{
-		IsCA:         false,
-		SerialNumber: clientSerialNumber,
-		Subject: pkix.Name{
-			Organization: []string{org},
-			SerialNumber: clientSerialNumber.String(),
-		},
-		NotBefore: notBefore,
-		NotAfter:  notAfter,
+	fields.CommonName = fields.CommonName + " Root CA"
 
-		KeyUsage: x509.KeyUsageDigitalSignature,
-		ExtKeyUsage: []x509.ExtKeyUsage{
-			x509.ExtKeyUsageClientAuth,
-		},
-		BasicConstraintsValid: true,
+	rootCA := caTemplate
+	rootCA.Subject = pkix.Name{
+		CommonName:   fields.CommonName,
+		Organization: fields.Organization,
 	}
 
-	for _, h := range hosts {
-		if ip := net.ParseIP(h); ip != nil {
-			leafTemplate.IPAddresses = append(leafTemplate.IPAddresses, ip)
-			clientTemplate.IPAddresses = append(clientTemplate.IPAddresses, ip)
+	cert, err := genCert(&rootCA, nil)
+	return cert, err
+}
+
+// GenerateIntermediate generates an intermediate certificate, returns cert
+func GenerateIntermediate(fields pkix.Name, signCA *Cert) (*Cert, error) {
+	if fields.CommonName == "" {
+		fields.CommonName = fields.Organization[0]
+	}
+
+	fields.CommonName = fields.CommonName + " Intermediate CA"
+
+	intermediateCA := caTemplate
+	intermediateCA.Subject = pkix.Name{
+		CommonName:   fields.CommonName,
+		Organization: fields.Organization,
+	}
+	intermediateCA.MaxPathLen = 1
+
+	cert, err := genCert(&intermediateCA, signCA)
+	return cert, err
+}
+
+// GenerateServer generates an server certificate, returns cert
+func GenerateServer(fields pkix.Name, signCA *Cert, hosts []string) (*Cert, error) {
+	if fields.CommonName == "" {
+		fields.CommonName = fields.Organization[0]
+	}
+
+	serverCA := caTemplate
+	serverCA.Subject = pkix.Name{
+		CommonName:   fields.CommonName,
+		Organization: fields.Organization,
+	}
+
+	for _, host := range hosts {
+		if ip := net.ParseIP(host); ip != nil {
+			serverCA.IPAddresses = append(serverCA.IPAddresses, ip)
 		} else {
-			leafTemplate.DNSNames = append(leafTemplate.DNSNames, h)
-			clientTemplate.DNSNames = append(clientTemplate.DNSNames, h)
+			serverCA.DNSNames = append(serverCA.DNSNames, host)
 		}
 	}
 
-	root, key, err := genCert(&rootTemplate, &rootTemplate, nil)
-	if err != nil {
-		return nil, err
-	}
-	leaf, _, err := genCert(&leafTemplate, &rootTemplate, key)
-	if err != nil {
-		return nil, err
-	}
-	client, _, err := genCert(&clientTemplate, &rootTemplate, key)
-	if err != nil {
-		return nil, err
-	}
-	return &Certs{
-		Root:   root,
-		Leaf:   leaf,
-		Client: client,
-	}, nil
+	cert, err := genCert(&serverCA, signCA)
+	return cert, err
 }
 
-func genCert(leaf *x509.Certificate, parent *x509.Certificate, signingKey *ecdsa.PrivateKey) (*Cert, *ecdsa.PrivateKey, error) {
-	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		return nil, nil, err
-	}
-	if leaf == parent {
-		if signingKey != nil {
-			return nil, nil, fmt.Errorf("signing key must be nil when generating root cert")
-		}
-		signingKey = key
+// GenerateClient generates an client certificate, returns cert
+func GenerateClient(fields pkix.Name, signCA *Cert, hosts []string) (*Cert, error) {
+	if fields.CommonName == "" {
+		fields.CommonName = fields.Organization[0]
 	}
 
-	cert := new(Cert)
-	derBytes, err := x509.CreateCertificate(rand.Reader, leaf, parent, &key.PublicKey, signingKey)
+	clientCA := caTemplate
+	clientCA.Subject = pkix.Name{
+		CommonName:   fields.CommonName,
+		Organization: fields.Organization,
+	}
+
+	for _, host := range hosts {
+		if ip := net.ParseIP(host); ip != nil {
+			clientCA.IPAddresses = append(clientCA.IPAddresses, ip)
+		} else {
+			clientCA.DNSNames = append(clientCA.DNSNames, host)
+		}
+	}
+
+	cert, err := genCert(&clientCA, signCA)
+	return cert, err
+}
+
+func genCert(child *x509.Certificate, parent *Cert) (*Cert, error) {
+	var signingKey interface{}
+
+	// Serial Number
+	serialNumber, lastFour, err := randomSerialNumber()
 	if err != nil {
-		return nil, nil, fmt.Errorf("Failed to create certificate: %s", err)
+		return nil, err
+	}
+
+	// Flex in Serial Number to Certificate
+	child.SerialNumber = serialNumber
+	child.Subject.CommonName = fmt.Sprintf("%s (%s)", child.Subject.CommonName, *lastFour)
+	child.Subject.SerialNumber = serialNumber.String()
+
+	// Generate New Key
+	key, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	// Isolate Signing Key
+	if parent == nil {
+		parent = new(Cert)
+		parent.Certificate = *child
+		signingKey = key
+	} else {
+		signingKey, err = x509.ParsePKCS8PrivateKey(parent.Private.Bytes)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Create Certificate
+	cert := new(Cert)
+	derBytes, err := x509.CreateCertificate(rand.Reader, child, &parent.Certificate, &key.PublicKey, signingKey)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create certificate: %s", err)
 	}
 	cert.Public = &pem.Block{Type: "CERTIFICATE", Bytes: derBytes}
 	buf := new(bytes.Buffer)
 	if err := pem.Encode(buf, cert.Public); err != nil {
-		return nil, nil, fmt.Errorf("failed to write data to cert.pem: %s", err)
+		return nil, fmt.Errorf("failed to write data to cert.pem: %s", err)
 	}
 	cert.PublicBytes = make([]byte, buf.Len())
 	copy(cert.PublicBytes, buf.Bytes())
@@ -155,13 +209,15 @@ func genCert(leaf *x509.Certificate, parent *x509.Certificate, signingKey *ecdsa
 
 	b, err := x509.MarshalPKCS8PrivateKey(key)
 	if err != nil {
-		return nil, nil, fmt.Errorf("Unable to marshal ECDSA private key: %v", err)
+		return nil, fmt.Errorf("Unable to marshal ECDSA private key: %v", err)
 	}
 	cert.Private = &pem.Block{Type: "PRIVATE KEY", Bytes: b}
 	if err := pem.Encode(buf, cert.Private); err != nil {
-		return nil, nil, fmt.Errorf("failed to encode key data: %s", err)
+		return nil, fmt.Errorf("failed to encode key data: %s", err)
 	}
 	cert.PrivateBytes = make([]byte, buf.Len())
 	copy(cert.PrivateBytes, buf.Bytes())
-	return cert, key, nil
+
+	cert.Certificate = *child
+	return cert, nil
 }
--- main.go
+++ main.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-	"flag"
+	"crypto/x509/pkix"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
-	"strings"
-	"time"
 
-	gencert "github.com/meterup/generate-cert/lib"
+	gencert "github.com/jnovack/generate-cert/lib"
 )
 
 func writeCert(c *gencert.Cert, rootFilename string) error {
-	pubkey := rootFilename + ".pem"
+	pubkey := "temp/" + rootFilename + ".pem"
 	if err := ioutil.WriteFile(pubkey, c.PublicBytes, 0666); err != nil {
 		return err
 	}
-	privkey := rootFilename + ".key"
+	privkey := "temp/" + rootFilename + ".key"
 	if err := ioutil.WriteFile(privkey, c.PrivateBytes, 0600); err != nil {
 		return err
 	}
@@ -25,29 +22,69 @@ func writeCert(c *gencert.Cert, rootFilename string) error {
 }
 
 func main() {
-	version := flag.Bool("version", false, "Print the version string and exit")
-	host := flag.String("host", "", "Comma-separated hostnames and IPs to generate a certificate for")
-	validFor := flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for")
-	organization := flag.String("organization", "Acme Co", "Company to issue the cert to")
-	flag.Parse()
-	if *version {
-		fmt.Fprintf(os.Stderr, "generate-cert version %s\n", gencert.Version)
-		os.Exit(0)
+	// version := flag.Bool("version", false, "Print the version string and exit")
+	// host := flag.String("host", "", "Comma-separated hostnames and IPs to generate a certificate for")
+	// validFor := flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for")
+	// organization := flag.String("organization", "Acme", "Company to issue the cert to")
+	// flag.Parse()
+	// if *version {
+	// 	fmt.Fprintf(os.Stderr, "generate-cert version %s\n", gencert.Version)
+	// 	os.Exit(0)
+	// }
+
+	values := pkix.Name{
+		Organization: []string{"ACME Company"},
 	}
 
-	hosts := strings.Split(*host, ",")
-	certs, err := gencert.Generate(hosts, *organization, *validFor)
+	// Root CA
+	rootCA, err := gencert.GenerateRoot(values)
 	if err != nil {
 		log.Fatal(err)
 	}
+	if err := writeCert(rootCA, "root"); err != nil {
+		log.Fatal(err)
+	}
 
-	if err := writeCert(certs.Root, "root"); err != nil {
+	// Intermediate CA for Servers
+	svrCA, err := gencert.GenerateIntermediate(values, rootCA)
+	if err != nil {
 		log.Fatal(err)
 	}
-	if err := writeCert(certs.Leaf, "leaf"); err != nil {
+	if err := writeCert(svrCA, "intermediate-server"); err != nil {
 		log.Fatal(err)
 	}
-	if err := writeCert(certs.Client, "client"); err != nil {
+
+	// Intermediate CA for Clients
+	cliCA, err := gencert.GenerateIntermediate(values, rootCA)
+	if err != nil {
 		log.Fatal(err)
 	}
+	if err := writeCert(cliCA, "intermediate-client"); err != nil {
+		log.Fatal(err)
+	}
+
+	// Server Certs
+	for i := 1; i < 4; i++ {
+		name := fmt.Sprintf("server%d.local", i)
+		server, err := gencert.GenerateServer(values, svrCA, []string{name})
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := writeCert(server, name); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Client Certs
+	for i := 1; i < 4; i++ {
+		name := fmt.Sprintf("client%d", i)
+		client, err := gencert.GenerateClient(values, cliCA, []string{name})
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := writeCert(client, name); err != nil {
+			log.Fatal(err)
+		}
+	}
+
 }
